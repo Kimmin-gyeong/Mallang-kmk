486e748a1581b878b584b8c6b88aabcb685a6f2f
KMK <3734304@naver.com>
2026-03-24 17:43:38 +0900

    보정 요청 메세지 보내기 (추합test)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,15 +13,24 @@ java {
 	}
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ src/main/java/com/smhrd/malang/controller/preview/ChatController.java
@@ -0,0 +1,33 @@
+package com.smhrd.malang.controller.preview;
+
+import com.smhrd.malang.dto.preview.ChatRequest;
+import com.smhrd.malang.domain.ChatMessage;
+import com.smhrd.malang.repository.ChatMessageRepository;
+import com.smhrd.malang.service.preview.ChatService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class ChatController {
+    private final ChatService chatService;
+    private final ChatMessageRepository chatMessageRepository;
+
+    public ChatController(ChatService chatService, ChatMessageRepository chatMessageRepository) {
+        this.chatService = chatService;
+        this.chatMessageRepository = chatMessageRepository;
+    }
+
+    @GetMapping("/chat")
+    public String chatPage() {
+        return "preview/chatView";
+    }
+
+    @PostMapping("/chat/correct")
+    public String correct(ChatRequest request, Model model) {
+        ChatMessage result = chatService.createCorrection(request);
+        model.addAttribute("result",result);
+        return "preview/chatView";
+    }
+}
