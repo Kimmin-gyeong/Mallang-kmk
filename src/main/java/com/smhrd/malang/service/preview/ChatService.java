8d7de34f320c0ae9b1a2374a3003638190ec8bc8
Yeong Kim <litsync1@gmail.com>
2026-03-26 16:23:21 +0900

    잠금기능 업데이트

--- src/main/java/com/smhrd/malang/service/preview/ChatService.java
+++ src/main/java/com/smhrd/malang/service/preview/ChatService.java
@@ -3,27 +3,33 @@ package com.smhrd.malang.service.preview;
 import com.smhrd.malang.dto.preview.ChatRequest;
 import com.smhrd.malang.domain.ChatMessage;
 import com.smhrd.malang.repository.ChatMessageRepository;
+import com.smhrd.malang.repository.LockedWordRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ChatService {
 
     private final ChatMessageRepository chatMessageRepository;
+    private final LockedWordRepository lockedWordRepository; //단어보호정보를 담기위해 연결
 
-    public ChatService(ChatMessageRepository chatMessageRepository){
+    public ChatService(ChatMessageRepository chatMessageRepository, LockedWordRepository lockedWordRepository){
         this.chatMessageRepository = chatMessageRepository;
+        this.lockedWordRepository = lockedWordRepository;
     }
 
-    // 임시
+    // 임시 : 보정 요청 기능
     public ChatMessage createCorrection(ChatRequest request) {
+
         ChatMessage msg = new ChatMessage();
         msg.setRoomId(1); // 테스트용 임시값
         msg.setSenderId(1); // 테스트용 임시값
 //        msg.setPersonaId(1); // 테스트용 임시값
         msg.setOriginalMsg(request.getOriginalMsg());
-
         msg.setFinalMsg("");
 
         return chatMessageRepository.save(msg);
     }
+
 }
