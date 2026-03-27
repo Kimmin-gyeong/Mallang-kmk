5ce768c75da0b46ea52a745963dfb2a20b7fa76d
KMK <3734304@naver.com>
2026-03-27 10:41:41 +0900

    26.03.27 깃허브 연습2

--- src/main/java/com/smhrd/malang/MalangApplication.java
+++ src/main/java/com/smhrd/malang/MalangApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing //auditing 기능활성화 (엔티티)
 @SpringBootApplication
 public class MalangApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(MalangApplication.class, args);
 	}
